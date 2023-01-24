e027669cdfdb48f87a521c6bb7b933ac14feef5c
Ariana Belgrado <belgradoariana@gmail.com>
2023-01-24 17:59:43 -0300

    Agregar archivos de proyecto.

---
+++ WebApiControlStock/Data/DbStockContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace WebApiControlStock.Data
+{
+    public class DbStockContext : DbContext
+    {
+        public DbStockContext(DbContextOptions<DbStockContext> options) : base(options) { }
+
+    }
+}
